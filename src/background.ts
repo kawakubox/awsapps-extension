afb46d0b2ca3e3db2e4d71bc7f35cbada3131d15
kawakubox <shigefumi.kawakubo@gmail.com>
2022-03-05 18:23:03 +0900

    Remove console.log

--- src/background.ts
+++ src/background.ts
@@ -1,9 +1,7 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (tab.url.match(/https:\/\/.*\.console\.aws\.amazon\.com\/*/)) {
-        console.log("URL matched")
         if (changeInfo.status == "complete") {
             chrome.tabs.sendMessage(tabId, {url: tab.url}, (response) => {
-                console.log("response:" + response);
             });
         }
     }
